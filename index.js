6720eb4d7d819a525211d3a314cb45b5c388027d
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-01-20 18:06:13 +0200

    edd commentar

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // Завдання перетворити рядок 
 const string = 'hELlo THERE, hOW ARE yOu doINg?';
 console.log(string);
+// Зробити все маленькими літерами і використовуючи метод slice почати речення з другого елемента
 const newStringToLowerCase = string.toLowerCase().slice(1);
 console.log(newStringToLowerCase);
+// Зробити першу літеру великою
 const firstLetter = string[0].toUpperCase();
 console.log(firstLetter);
+// Перша літера велика інші маленькі
 const newString = firstLetter + newStringToLowerCase;
 console.log(newString);
